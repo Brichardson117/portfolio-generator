aa2b44f6f58634f26dafe50cd6d6b8cd82e241cc
Brittney <brittneyrichardson8@gmail.com>
2022-04-21 17:47:18 -0500

    added when for about section

--- app.js
+++ app.js
@@ -53,11 +53,26 @@ const promptUser = () => {
             }
         }
     },
+     {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+    },
     {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself!:'
-    }
+        message: 'Provide some information about yourself!:',
+        // similar to validate method used in file. passes object of answers given thus far as an object
+        when: ({ confirmAbout}) => {
+            if (confirmAbout) {
+                return true
+            } else {
+                return false;
+            }
+        }
+    },
+
 ]);
 };
 
