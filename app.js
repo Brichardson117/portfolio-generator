90d0b948370ed8e7d9b9e1c42586c2237fc57630
Brittney <brittneyrichardson8@gmail.com>
2022-04-21 17:19:42 -0500

    added profile and project question prompts and conditional to add multiple projects

--- app.js
+++ app.js
@@ -21,26 +21,90 @@ const inquirer = require('inquirer');
 
 inquirer 
 // can receive an array of objects in its argument, known as the 'question' object
-// // the properties of 'question' object identify the type, name, and and question message of this particular question
-.prompt([
-    {
-        // 'input' will allow user to type answer to question
+ // // the properties of 'question' object identify the type, name, and and question message of this particular question
+const promptUser = () => {
+   
+    return inquirer.prompt([
+    { // 'input' will allow user to type answer to question
         type: 'input',
         name: 'name',
         message: 'What is your name?'
+    }, 
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github username'
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself!:'
     }
-])
-// answer object is returned as a Promise(tool for using asynchronus functions)
-.then(answers => console.log(answers));
+]);
+};
 
-// const printProfileData = profileDataArr => {
+const promptProject = portfolioData => {
+
+    // If there's no 'projects' an array property, create one
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+    } 
     
-//     for (let i = 0; i < profileDataArr.length; i += 1) { 
-//     console.log(profileDataArr[i]);
-//     }
-//     console.log('================');
 
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+    console.log('============ Add a New Project ==============');
 
-// printProfileData(profileDataArgs);
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What's the name of your project?"
+        },
+        {
+            // list possible answers in array
+            type: 'checkbox',
+            name: 'Languages',
+            message: 'What did you build your project with? (check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the Github link to your project. (REQUIRED)'
+        },
+        {
+            // confirm type is a boolean, can recieve yes or no(true or false) responses
+            type:'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type:'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+
+        }
+    ])
+      // used array method push() to place projectData from inquirer into the new projects array we just created
+      .then(projectData => {
+        portfolioData.projects.push(projectData);
+        // evaluating user response to whether they wish to add more projects.  without portfolioData as an argument, a new project will be initialized and existing project data will be lost
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } 
+        // if userd declined to add more projects, will return portfolio daat
+        else {
+            return portfolioData;
+        }
+    })
+};
+
+
+// answer object is returned as a Promise(tool for using asynchronus functions)
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
+
